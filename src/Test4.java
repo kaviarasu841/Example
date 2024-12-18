bc0f5ded12e9b7b68f6965a1d031bb48993ae482
kaviarasu1234 <kaviarasu.s@dxc.com>
2024-12-18 16:47:42 +0530

    fourth commit

---
+++ src/Test4.java
@@ -0,0 +1,11 @@
+public class Test4 {
+	public static void main(String args[]) {
+		String hungry = "yes";
+		if(hungry == "yes")
+			System.out.println("eat something....");
+		else {
+			System.out.println("Do work...");
+			System.out.println("Do work...");
+		}
+	}
+}
