11483371e8d31aeee759f17c19985868d683de1f
Kaviarasu S <75796035+kaviarasu841@users.noreply.github.com>
2024-12-18 17:53:46 +0530

    Update Test1 3.java

--- src/Test1.java
+++ src/Test1.java
@@ -7,6 +7,7 @@ public class Test1 {
 			System.out.println("Do work...");
 			System.out.println("Do work...");
 			System.out.println("Do work...");
+			System.out.println("Do work...");
 		}
 	}
 }
